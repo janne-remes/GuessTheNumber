2e531dfe21f201354b58a875bd9dc933fbdb71fc
janne-remes <janneremes@outlook.com>
2020-05-05 18:01:16 +0300

    Numerotarkastus, arvatut numerot & arvauskerrat OK

--- main.js
+++ main.js
@@ -1,8 +1,50 @@
 var secretNumber = Math.floor(Math.random() * 100) + 1;
-
-//console.log(secretNumber);
+var howManyGuesses = 0;
+var guessedNumbersArray = [];
 
 function usersGuess(){
+    
     var guessedNumber = parseInt(document.getElementById("guessField").value);
-    console.log(guessedNumber);
+    
+    if (guessedNumber == secretNumber){
+        
+        howManyGuesses++;
+
+        document.getElementById("messages").innerHTML = "Arvasit luvun oikein! " +
+        "Käytit " + howManyGuesses + " arvauskertaa.";
+        
+        guessedNumbersArray.push(guessedNumber);
+    }
+
+    else if (isNaN(guessedNumber)){
+        
+        document.getElementById("messages").innerHTML = "Syötä NUMERO!";
+    }
+
+    else if (guessedNumber < 1 || guessedNumber > 100){
+        
+        document.getElementById("messages").innerHTML = "Anna luku väliltä 1 - 100.";
+    }
+
+    else if (secretNumber > guessedNumber){
+        
+        howManyGuesses++;
+
+        document.getElementById("messages").innerHTML = "Arvottu luku on suurempi kuin "
+        + guessedNumber + ".";
+
+        guessedNumbersArray.push(guessedNumber);
+    }
+
+    else if (secretNumber < guessedNumber){
+        
+        howManyGuesses++;
+
+        document.getElementById("messages").innerHTML = "Arvottu luku on pienempi kuin "
+        + guessedNumber + ".";
+
+        guessedNumbersArray.push(guessedNumber);
+    }
+
+    document.getElementById("wrongGuesses").innerHTML = guessedNumbersArray;
 }
