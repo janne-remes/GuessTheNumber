6819431257f618ce7c14326ac78b5f2769eadd8b
janne-remes <janneremes@outlook.com>
2020-05-05 10:44:46 +0300

    Luvun arvonta, arvauksen muutos integeriksi.

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-var secretNumber;
+var secretNumber = Math.floor(Math.random() * 100) + 1;
 
-function start(){
-    secretNumber = Math.floor(Math.random() * 100) + 1;
+//console.log(secretNumber);
+
+function usersGuess(){
+    var guessedNumber = parseInt(document.getElementById("guessField").value);
+    console.log(guessedNumber);
 }
-
-console.log(secretNumber);
